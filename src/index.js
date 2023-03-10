7403c4bc084df24d1e318ebf18b0af5d08fb1d24
CFeeney333 <c.feeney333@gmail.com>
2023-03-10 15:58:46 +0000

    Create backbone js for main application and modify html to use it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+import {createHome} from "./home";
+import {createMenu} from "./menu";
+import {createContact} from "./contact";
+
+function createHeader() {
+    return document.createElement("div");
+}
+
+function createContent() {
+    const content = document.createElement("div");
+    content.appendChild(createHome());
+    content.appendChild(createMenu());
+    content.appendChild(createContact());
+    return content;
+}
+
+function loadPage() {
+    const page = document.querySelector('#content');
+    page.appendChild(createHeader());
+    page.appendChild(createContent());
+}
+
+loadPage();
