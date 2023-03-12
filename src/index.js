0d0963cfc9b651dd7789525531d72d1254247648
CFeeney333 <c.feeney333@gmail.com>
2023-03-12 20:59:28 +0000

    Add tabs to the top of the page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,29 @@ import {createMenu} from "./menu";
 import {createContact} from "./contact";
 
 function createHeader() {
-    return document.createElement("div");
+    const container = document.createElement("div");
+    const navList = document.createElement("ul");
+
+    const homeTab = document.createElement("li");
+    homeTab.id = 'home';
+    homeTab.classList.add("tab");
+    homeTab.textContent = "HOME";
+    navList.appendChild(homeTab);
+
+    const menuTab = document.createElement("li");
+    menuTab.id = 'menu';
+    menuTab.classList.add("tab");
+    menuTab.textContent = "MENU";
+    navList.appendChild(menuTab);
+
+    const contactTab = document.createElement("li");
+    contactTab.id = 'contact';
+    contactTab.classList.add("tab");
+    contactTab.textContent = "CONTACT";
+    navList.appendChild(contactTab);
+
+    container.appendChild(navList);
+    return container;
 }
 
 function createContent() {
