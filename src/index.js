7626cb1c9e0b2000aa8979e89cf2002e23051fe3
CFeeney333 <c.feeney333@gmail.com>
2023-03-12 21:23:54 +0000

    Implement basic tab switching functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import {createHome} from "./home";
 import {createMenu} from "./menu";
 import {createContact} from "./contact";
 
+const content = document.createElement("div");
+
 function createHeader() {
     const container = document.createElement("div");
     const navList = document.createElement("ul");
@@ -10,36 +12,64 @@ function createHeader() {
     homeTab.id = 'home';
     homeTab.classList.add("tab");
     homeTab.textContent = "HOME";
+    homeTab.addEventListener('click', onTabClick);
     navList.appendChild(homeTab);
 
     const menuTab = document.createElement("li");
     menuTab.id = 'menu';
     menuTab.classList.add("tab");
     menuTab.textContent = "MENU";
+    menuTab.addEventListener('click', onTabClick);
     navList.appendChild(menuTab);
 
     const contactTab = document.createElement("li");
     contactTab.id = 'contact';
     contactTab.classList.add("tab");
     contactTab.textContent = "CONTACT";
+    contactTab.addEventListener('click', onTabClick);
     navList.appendChild(contactTab);
 
     container.appendChild(navList);
     return container;
 }
 
-function createContent() {
-    const content = document.createElement("div");
+function onTabClick(e) {
+    const id = e.target.id;
+    if (id === 'home') {
+        loadHome();
+    } else if (id === 'menu') {
+        loadMenu();
+    } else if (id === 'contact') {
+        loadContact();
+    }
+}
+
+function loadHome() {
+    clearContent();
     content.appendChild(createHome());
+}
+
+function loadMenu() {
+    clearContent();
     content.appendChild(createMenu());
+}
+
+function loadContact() {
+    clearContent();
     content.appendChild(createContact());
-    return content;
+}
+
+function clearContent() {
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
 }
 
 function loadPage() {
     const page = document.querySelector('#content');
     page.appendChild(createHeader());
-    page.appendChild(createContent());
+    page.appendChild(content);
+    loadHome();
 }
 
 loadPage();
