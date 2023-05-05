6d4386c3708d3347128f1b99ff685ee6d485a430
CFeeney333 <c.feeney333@gmail.com>
2023-05-05 23:33:48 +0100

    Create basic lists template for menu

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,51 @@
 function createMenu() {
     const menuElement = document.createElement("div");
-    menuElement.textContent = "MENU CONTENT";
+
+    const title = document.createElement("h1");
+    title.textContent = "MENU";
+
+    const line = document.createElement("div");
+    line.classList.add("line");
+
+    const pizzaHeading = document.createElement("h3");
+    pizzaHeading.textContent = "PIZZAS";
+
+    const pizzaList = document.createElement("ul");
+    const pizzas = [
+        "margarita",
+        "pepperoni",
+        "hawaii",
+        "fungi"
+    ];
+    pizzas.forEach((name) => pizzaList.appendChild(createListItem(name)));
+
+    const otherHeading = document.createElement("h3");
+    otherHeading.textContent = "OTHER STUFF";
+
+    const otherList = document.createElement("ul");
+    const otherStuff = [
+        "other1",
+        "other2",
+        "other3",
+        "other4",
+        "other5"
+    ];
+    otherStuff.forEach((name) => otherList.appendChild(createListItem(name)));
+
+    menuElement.appendChild(title);
+    menuElement.appendChild(line);
+    menuElement.appendChild(pizzaHeading);
+    menuElement.appendChild(pizzaList);
+    menuElement.appendChild(otherHeading);
+    menuElement.appendChild(otherList);
+
     return menuElement;
+}
+
+function createListItem(text) {
+    const ul = document.createElement("ul");
+    ul.textContent = text;
+    return ul;
 }
 
 export function loadMenu() {
