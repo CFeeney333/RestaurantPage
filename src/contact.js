aadda82d0de0c1fa98162e3b060d3dabf75963c7
CFeeney333 <c.feeney333@gmail.com>
2023-05-16 21:58:15 +0100

    Add content to contact tab

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,22 @@
 function createContact() {
     const contactElement = document.createElement("div");
-    contactElement.textContent = "CONTACT CONTENT";
+
+    const menuElement = document.createElement("div");
+
+    const title = document.createElement("h1");
+    title.textContent = "CONTACT";
+
+    const line = document.createElement("div");
+    line.classList.add("line");
+
+
+    const contact = document.createElement("p");
+    contact.textContent = "089 435 8382";
+
+    contactElement.appendChild(title);
+    contactElement.appendChild(line);
+    contactElement.appendChild(contact);
+
     return contactElement;
 }
 
